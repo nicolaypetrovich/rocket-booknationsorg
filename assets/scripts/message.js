86525026ad4659e0513515e3337f5458f441c716
Yevhen Synytsia <sinizaevgen@gmail.com>
2018-07-10 18:49:36 +0300

    Make desk page

---
+++ assets/scripts/message.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+	const btnMess = document.querySelector('.block_discus');
+	const contMess = document.querySelector('.wrap_message');
+
+	btnMess.addEventListener('click', function(event) {
+
+		console.log(btnMess);
+		// event.preventDefault();
+
+		// contMess.forEach( element => {
+
+  //           if( element.classList.contains('non_activ') ) {
+  //               element.classList.toggle('non_activ');
+  //               return;
+  //           }
+  //       })
+  //       event.target.classList.add('red_text');
+   })  
+}	
