8b7d67afbac18c9f00a841182c110480d940479f
Unknown <danilderevyanchenko@gmail.com>
2018-07-11 18:05:44 +0300

    Bug fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -306,6 +306,18 @@ $(document).ready(function () {
             $('.forum_editable_field label #mce_0_ifr').css('borderColor','red')
         }
     });
+
+        function validate(evt) {
+          var theEvent = evt || window.event;
+          var key = theEvent.keyCode || theEvent.which;
+          key = String.fromCharCode( key );
+          var regex = /[0-9]|\./;
+          if( !regex.test(key) ) {
+            theEvent.returnValue = false;
+            if(theEvent.preventDefault) theEvent.preventDefault();
+          }
+        }
+
 });
 
 
