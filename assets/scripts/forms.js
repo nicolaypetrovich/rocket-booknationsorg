7ab385b132e61fb29326785c9378bbb4eb74e986
Hozhiy <gun.dimonchik@gmail.com>
2018-08-09 12:49:20 +0300

    fix

--- assets/scripts/forms.js
+++ assets/scripts/forms.js
@@ -91,7 +91,7 @@ $(document).ready(function () {
                     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
                     if ( !re.test(String(email.value).toLowerCase()) ){
-                        showValidateError( email, 'test@test.com' );
+                        showValidateError( email, '' );
                         submit = false;
                     }
                 }
