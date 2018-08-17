b55a57517bed648620c51da1ceb866c8de42053a
Hozhiy <gun.dimonchik@gmail.com>
2018-08-17 17:57:17 +0300

    fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -260,17 +260,16 @@ $('.course-gallery').magnificPopup({
             }
         ]
     });
-/*
-    (function() {
-    if (window.pluso)if (typeof window.pluso.start == "function") return;
-    if (window.ifpluso==undefined) { window.ifpluso = 1;
+ (function() {
+      if (window.pluso)if (typeof window.pluso.start == "function") return;
+      if (window.ifpluso==undefined) { window.ifpluso = 1;
         var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
         s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
         s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
         var h=d[g]('body')[0];
         h.appendChild(s);
-    }});
-*/
+      }})();
+    
     // Datapicker
     
     $('#js-date-picker, .js-date-picker,.js-date-picker-reg').datepicker({
